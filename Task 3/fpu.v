fab16d8e493201fd8414faf80b21fbd1be43b942
Rishadd Ranjith <86703898+Rishadd@users.noreply.github.com>
2025-11-08 12:34:13 +0530

    Update and rename sample.txt to fpu.v

---
+++ Task 3/fpu.v
@@ -0,0 +1,11 @@
+//DO NOT MODIFY THE MODULE DEFINITION
+module fpu (
+  input [15:0] a, b,
+  input [3:0] aluctrl,
+  output [15:0] result,
+  output zero
+);
+
+//Add all your code here
+  
+endmodule
