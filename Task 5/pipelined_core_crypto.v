0047753fb41c137182d733da690530536f24e970
Rishadd Ranjith <86703898+Rishadd@users.noreply.github.com>
2025-11-09 09:53:44 +0530

    Rename module to crypto_coproc and update inputs

--- Task 5/pipelined_core_crypto.v
+++ Task 5/pipelined_core_crypto.v
@@ -1,7 +1,9 @@
 //DO NOT MODIFY THE MODULE DEFINITION
-module pipelind_core_crypto (
-  input clk,
-  input rst
+module crypto_coproc (
+    input [15:0] data_in,
+    input enc,
+    input dec,
+    output [15:0] data_out
 );
 
 //Add all your code here
